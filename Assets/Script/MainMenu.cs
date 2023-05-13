76b473abacf4c5b66b2fc2c68751f7d321f27cd9
Wichdan <114506363+Wichdan@users.noreply.github.com>
2023-05-13 14:50:30 +0700

    updated title screen
    title screen, dll

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] int selectedLevel;
-    [SerializeField] Button startGameButton;
     [SerializeField] bool isStartGame;
-    [SerializeField] GameObject TitleSceenPanel, MainMenuPanel;
+    [SerializeField] AudioSource Music;
+    [SerializeField] GameObject TitleSceenPanel, MainMenuPanel, NewGamePanel, OptionPanel, ControlPanel, VolumePanel, SystemPanel;
 
     private void Start()
     {
-        //startGameButton.interactable = false;
+        Music.Play();
     }
 
     private void Update()
@@ -24,26 +23,67 @@ public class MainMenu : MonoBehaviour
 
         if(isStartGame){
             MainMenuPanel.SetActive(true);
-            TitleSceenPanel.SetActive(false);
+            OptionPanel.SetActive(false);
+            VolumePanel.SetActive(false);
+            SystemPanel.SetActive(false);
         }
+
+        TitleSceenPanel.SetActive(!isStartGame);
     }
 
-    public void SelectLevel(int level)
-    {
-        selectedLevel = level;
-        EnableStartGame();
-    }
-
-    public void EnableStartGame(){
-        startGameButton.interactable = true;
-    }
-
-    public void StartGame()
-    {
-        if(selectedLevel < 1) return;
-        SceneManager.LoadScene(selectedLevel);
+    public void ToggleStartGame(){
+        isStartGame = !isStartGame;
     }
 
     public void ChangeStartGame(bool isStartGame) => this.isStartGame = isStartGame;
+
+    public void ChangeScene(int scene){
+        SceneManager.LoadScene(scene);
+    }
+
+    public void BackToMainMenu()
+    {
+        NewGamePanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        ControlPanel.SetActive(false);
+        VolumePanel.SetActive(false);
+        SystemPanel.SetActive(false);
+    }
+    public void OpenNewGame()
+    {
+        NewGamePanel.SetActive(true);
+    }
+
+    public void OpenOptions()
+    {
+        OptionPanel.SetActive(true);
+        ControlPanel.SetActive(true);
+        VolumePanel.SetActive(false);
+        SystemPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ControlOptions()
+    {
+        ControlPanel.SetActive(true);
+        VolumePanel.SetActive(false);
+        SystemPanel.SetActive(false);
+    }
+    public void VolumeOptions()
+    {
+        ControlPanel.SetActive(false);
+        VolumePanel.SetActive(true);
+        SystemPanel.SetActive(false);
+    }
+    public void SystemOptions()
+    {
+        ControlPanel.SetActive(false);
+        VolumePanel.SetActive(false);
+        SystemPanel.SetActive(true);
+    }
     
 }
